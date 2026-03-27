3d1273adbfffd898ca4b8dfcb77be2a1eb6a1011
tesar27 <tesar.public@gmail.com>
2026-03-27 22:44:15 +0100

    navtab custom implementation

--- lib/app/navigation/nav_tab.dart
+++ lib/app/navigation/nav_tab.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 
 class NavTab {
-  const NavTab({
-    required this.label,
-    required this.icon,
-  });
+  const NavTab({required this.label, required this.icon});
 
   final String label;
   final IconData icon;
