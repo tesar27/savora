dfe2321aacd6a014a700e333c211d7555ccbd877
tesar27 <tesar.public@gmail.com>
2026-04-24 16:40:55 +0200

    feat: setting up Appwrite SDK

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,23 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:supabase_flutter/supabase_flutter.dart';
+import 'config/appwrite_config.dart';
+import 'package:appwrite/appwrite.dart';
 import 'app/app.dart';
 
-void main() {
-  runApp(const SavoraApp());
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+  // 2. Initialize Supabase
+  // await Supabase.initialize(
+  //   url: dotenv.env['SUPABASE_URL']!,
+  //   anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  // );
+  WidgetsFlutterBinding.ensureInitialized();
+  Client()
+    ..setEndpoint(AppwriteConfig.endpoint)
+    ..setProject(AppwriteConfig.projectId)
+    ..setSelfSigned(
+      status: false,
+    ); // For development only, allows self-signed certificates
+  runApp(SavoraApp());
 }
